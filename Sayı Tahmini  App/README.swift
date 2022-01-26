b8bf8af1a2949c6a7654e7f97f79b991819f11c9
S.Tunahan ÜSTÜNTEPE <86594390+sTunahan@users.noreply.github.com>
2022-01-26 08:56:12 +0300

    Update README.swift

--- Sayı Tahmini  App/README.swift
+++ Sayı Tahmini  App/README.swift
@@ -1,7 +1,1 @@
-/// KULLANILANLAR
-/// Auto Layout
-/// Navigation Controller (segue and Code)
-///  Show segue ve performSegeu kullanıldı
 
-/// Uygulama Çoklu Ekran saglamak  ıcın resımlerın 3 boyutuda eklenmiştir.
-/// Konumlandırmaları  Ekran boyutuna gore ayarlama İşlemi Yapılacaktır.
